7874293fd2e5fd43548d96377fa543d49ee864bc
LUCAMA <luca.manini@scuole.provincia.tn.it>
2020-11-05 09:55:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# account
+data = """
+foo 123
+foo 444
+bar  90
+zot 100
+bar 666
+""".split("\n")
+def read(data):
+    rr = list()
+    for r in data:
+        if not r.strip():
+            continue
+        k, v = r.split()
+        v = int(v)
+        rr.append((k,v))
+    return rr 
+
+rr = read(data)
+
+
+
